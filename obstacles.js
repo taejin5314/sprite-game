c58cf998dbaeace2c611f5d032181fd97ae1023c
taejin <taejin5314@gmail.com>
2021-04-26 15:57:29 -0400

    add obstacles image

--- obstacles.js
+++ obstacles.js
@@ -6,10 +6,17 @@ class Obstacle {
     this.y = 0;
     this.width = 30;
     this.height = 30;
+    this.snail = new Image();
+    this.random = Math.floor(Math.random() * 3);
+    // console.log(this.random);
+    if (this.random === 0) this.snail.src = 'greenSnail.png';
+    else if (this.random === 1) this.snail.src = 'blueSnail.png';
+    else if (this.random === 2) this.snail.src = 'redSnail.png';
   }
   draw() {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.snail, this.x - 2, this.y);
   }
   update() {
     this.y += 2;
